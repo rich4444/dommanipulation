f302f54f34f1cfb05448fedb3b53880e681a9280
Lazy <ricardog.n@hotmail.com>
2024-12-08 12:42:04 -0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,27 @@
+//? LOOPS
+
+// for / while / foreach / for in
+
+//? SELECTION
+
+// Elements Selectors
+// Query Selectors
+
+//? EVENTS
+
+// Add event listener
+// function, anonymous function, arrow functions
+
+//? MODIFY
+
+// Inner HTML and Inner Text
+// Classes and IDs (and other attributes)
+
+//? CREATE
+
+// Create elements
+// Append
+
+//? DELETE
+
+// Delete elements
